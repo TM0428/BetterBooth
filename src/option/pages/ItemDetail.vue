cfcec504d124eaf7d4f03280c09b27a8f0520003
TM0428 <55181masui@seiko.ac.jp>
2024-03-19 17:40:04 +0900

    表示部分の改善

--- src/option/pages/ItemDetail.vue
+++ src/option/pages/ItemDetail.vue
@@ -1,122 +1,122 @@
 <template>
     <v-app class="content pa-0 ma-0">
-        <!--title-->
-        <div class="text-h4 ma-2 py-2 font-bold">{{ data.name }}</div>
+        <div class="center-margin">
+            <!--title-->
+            <div class="text-h4 ma-2 py-2 font-bold">{{ data.name }}</div>
 
-        <v-container class="mx-lg-2 px-2 mx-sm-4">
-            <v-row class="mx-sm-4">
-                <v-col cols="12" sm="12" md="8" lg="6" xl="4">
-                    <!--タグの追加-->
-                    <v-combobox
-                        class="ma-2"
-                        v-model="data.tags"
-                        :items="data.tags"
-                        chips
-                        clearable
-                        multiple
-                        label="tags"
-                        variant="solo"
-                    >
-                    </v-combobox>
-                </v-col>
-            </v-row>
-            <v-row class="mx-sm-4">
-                <v-col cols="12" sm="12" md="8" lg="6" xl="4">
-                    <v-carousel class="bg-grey-lighten-2">
-                        <v-carousel-item
-                            v-for="image in data.images"
-                            :src="image.original"
-                            @click="openPopup(image.original)"
-                        ></v-carousel-item>
-                    </v-carousel>
-                </v-col>
-            </v-row>
-        </v-container>
-
-        <p
-            class="description text-body-1 mx-4"
-            v-html="formatDescription(data.description)"
-        ></p>
-        <p
-            class="additional-description text-body-1 mx-4"
-            v-if="data.additionalDescription"
-            v-html="formatDescription(data.additionalDescription)"
-        ></p>
-
-        <div class="new-buttons">
-            <v-container>
-                <v-row>
-                    <v-col cols="12" sm="6" md="4" lg="3" xl="2">
-                        <v-btn
-                            block
-                            @click="$router.go(-1)"
-                            :prepend-icon="mdiArrowLeftIcon"
-                            rounded="xl"
-                            size="large"
-                        >
-                            {{ $t("itemBackToList") }}
-                        </v-btn>
+            <v-container class="mx-lg-2 px-2 mx-sm-4">
+                <v-row class="mx-sm-4">
+                    <v-col cols="12" sm="12" md="12" lg="6" xl="6">
+                        <v-carousel class="bg-grey-lighten-2">
+                            <v-carousel-item
+                                v-for="image in data.images"
+                                :src="image.original"
+                                @click="openPopup(image.original)"
+                            ></v-carousel-item>
+                        </v-carousel>
                     </v-col>
-                    <v-col cols="12" sm="6" md="4" lg="3" xl="2">
-                        <a
-                            :href="data.url"
-                            target="_blank"
-                            style="text-decoration: none"
+                    <v-col cols="12" sm="12" md="12" lg="6" xl="6">
+                        <!--タグの追加-->
+                        <v-combobox
+                            class="ma-2"
+                            v-model="data.tags"
+                            :items="data.tags"
+                            chips
+                            clearable
+                            multiple
+                            label="tags"
+                            variant="solo"
                         >
-                            <v-btn
-                                block
-                                rounded="xl"
-                                size="large"
-                                :prepend-icon="mdiLinkIcon"
-                            >
-                                {{ $t("itemGotoLink") }}
-                            </v-btn>
-                        </a>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4" lg="3" xl="2">
-                        <a
-                            :href="data.shop.url"
-                            target="_blank"
-                            style="text-decoration: none"
-                        >
-                            <v-btn
-                                block
-                                rounded="xl"
-                                size="large"
-                                :prepend-icon="mdiLinkIcon"
-                            >
-                                {{ $t("itemGotoShop") }}
-                            </v-btn>
-                        </a>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4" lg="3" xl="2">
-                        <v-btn
-                            block
-                            :prepend-icon="mdiDownloadIcon"
-                            rounded="xl"
-                            size="large"
-                            @click="exportData"
-                        >
-                            {{ $t("itemExport") }}
-                        </v-btn>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4" lg="3" xl="2">
-                        <v-btn
-                            block
-                            :prepend-icon="mdiDeleteIcon"
-                            rounded="xl"
-                            size="large"
-                            @click="deleteItem"
-                        >
-                            {{ $t("itemDelete") }}
-                        </v-btn>
+                        </v-combobox>
                     </v-col>
                 </v-row>
             </v-container>
-        </div>
-        <div class="popup" v-if="popupImage" @click="closePopup">
-            <span class="popup-close" @click="closePopup">&times;</span>
-            <img :src="popupImage" alt="Popup Image" class="popup-image" />
+
+            <p
+                class="description text-body-1 mx-4"
+                v-html="formatDescription(data.description)"
+            ></p>
+            <p
+                class="additional-description text-body-1 mx-4"
+                v-if="data.additionalDescription"
+                v-html="formatDescription(data.additionalDescription)"
+            ></p>
+
+            <div class="new-buttons">
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" sm="6" md="4" lg="3" xl="2">
+                            <v-btn
+                                block
+                                @click="$router.go(-1)"
+                                :prepend-icon="mdiArrowLeftIcon"
+                                rounded="xl"
+                                size="large"
+                            >
+                                {{ $t("itemBackToList") }}
+                            </v-btn>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4" lg="3" xl="2">
+                            <a
+                                :href="data.url"
+                                target="_blank"
+                                style="text-decoration: none"
+                            >
+                                <v-btn
+                                    block
+                                    rounded="xl"
+                                    size="large"
+                                    :prepend-icon="mdiLinkIcon"
+                                >
+                                    {{ $t("itemGotoLink") }}
+                                </v-btn>
+                            </a>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4" lg="3" xl="2">
+                            <a
+                                :href="data.shop.url"
+                                target="_blank"
+                                style="text-decoration: none"
+                            >
+                                <v-btn
+                                    block
+                                    rounded="xl"
+                                    size="large"
+                                    :prepend-icon="mdiLinkIcon"
+                                >
+                                    {{ $t("itemGotoShop") }}
+                                </v-btn>
+                            </a>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4" lg="3" xl="2">
+                            <v-btn
+                                block
+                                :prepend-icon="mdiDownloadIcon"
+                                rounded="xl"
+                                size="large"
+                                @click="exportData"
+                            >
+                                {{ $t("itemExport") }}
+                            </v-btn>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4" lg="3" xl="2">
+                            <v-btn
+                                block
+                                :prepend-icon="mdiDeleteIcon"
+                                rounded="xl"
+                                size="large"
+                                @click="deleteItem"
+                            >
+                                {{ $t("itemDelete") }}
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </div>
+            <div class="popup" v-if="popupImage" @click="closePopup">
+                <span class="popup-close" @click="closePopup">&times;</span>
+                <img :src="popupImage" alt="Popup Image" class="popup-image" />
+            </div>
         </div>
     </v-app>
 </template>
@@ -242,6 +242,10 @@ export default {
     font-family: -apple-system, BlinkMacSystemFont, Avenir, "Helvetica Neue",
         "Segoe UI", Arial, "ヒラギノ角ゴ ProN", "Hiragino Kaku Gothic ProN",
         メイリオ, Meiryo, "ＭＳ Ｐゴシック", sans-serif !important;
+}
+.center-margin {
+    margin: 0 auto;
+    max-width: 1100px;
 }
 
 .font-bold {
