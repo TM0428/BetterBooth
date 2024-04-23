992185ec3fc08b89f1771334b5b4aa071824c866
t.m <takashi.orion.999.428@gmail.com>
2024-04-24 02:57:02 +0900

    カラーテーマの追加

--- src/popup/main.js
+++ src/popup/main.js
@@ -9,6 +9,22 @@ import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 import { mdiAccount } from "@mdi/js";
 import i18n from "./i18n";
 
+const LightTheme = {
+    dark: false,
+    colors: {
+        background: "#EEE",
+        surface: "#FFFFFF",
+        primary: "#ff4d50",
+        "primary-darken-1": "#3700B3",
+        secondary: "#03DAC6",
+        "secondary-darken-1": "#018786",
+        error: "#B00020",
+        info: "#2196F3",
+        disable: "#BDBDBD",
+        success: "#4CAF50",
+        warning: "#FB8C00"
+    }
+};
 const vuetify = createVuetify({
     components,
     directives,
@@ -20,6 +36,12 @@ const vuetify = createVuetify({
         },
         sets: {
             mdi
+        }
+    },
+    theme: {
+        defaultTheme: "LightTheme",
+        themes: {
+            LightTheme
         }
     }
 });
