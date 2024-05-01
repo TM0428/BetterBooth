3d31d2df8aed175a0bbdd1dd9dcb3f5055e9a4aa
t.m <takashi.orion.999.428@gmail.com>
2024-05-02 03:22:54 +0900

    画像が送れない問題の修正

--- src/option/pages/ItemDetail.vue
+++ src/option/pages/ItemDetail.vue
@@ -9,8 +9,8 @@
                     <v-col cols="12" sm="12" md="12" lg="6" xl="6">
                         <v-carousel class="bg-grey-lighten-2">
                             <v-carousel-item
-                                v-for="image in data.images"
-                                :key="image.original"
+                                v-for="(image, i) in data.images"
+                                :key="i"
                                 :src="image.original"
                                 @click="openPopup(image.original)"
                             ></v-carousel-item>
