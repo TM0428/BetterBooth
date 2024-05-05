6e7f05275915a813393f3bc3bbbbcf25223db8ea
t.m <takashi.orion.999.428@gmail.com>
2024-05-06 00:34:41 +0900

    ショップのデータを表示するカードの作成

--- src/option/components/ShopCard.vue
+++ src/option/components/ShopCard.vue
@@ -2,6 +2,7 @@
     <v-card
         class="d-flex flex-column"
         height="100%"
+        min-width="300px"
         color="surfaceContainerLow"
         :title="shop.name"
         :subtitle="shop.url"
@@ -11,16 +12,64 @@
                 <v-img :src="shop.thumbnail_url"></v-img>
             </v-avatar>
         </template>
-        <v-card-text> </v-card-text>
+        <v-card-text>
+            <v-list>
+                <v-list-item v-for="(item, i) in shop.add_url" :key="i">
+                    <template v-slot:prepend>
+                        <v-icon :icon="LinkIcon"></v-icon>
+                    </template>
+                    <v-list-item-content append-icon="mdi-delete">
+                        <v-list-item-title> {{ item.url }}</v-list-item-title>
+                    </v-list-item-content>
+                    <template v-slot:append>
+                        <v-icon :icon="mdiClose" @click="deleteUrl(i, item.url)"></v-icon>
+                    </template>
+                </v-list-item>
+            </v-list>
+        </v-card-text>
     </v-card>
 </template>
 
 <script>
+import LinkIcon from "./icons/LinkIcon.vue";
+import { mdiClose } from "@mdi/js";
 export default {
     props: {
-        shop: {
-            type: Object,
+        shop_name: {
+            type: String,
             required: true
+        }
+    },
+    data() {
+        return {
+            LinkIcon: LinkIcon,
+            mdiClose: mdiClose,
+            shop: {
+                thumbnail_url:
+                    "https://booth.pximg.net/c/128x128/users/4312177/icon_image/73dd12b3-8562-4271-a4f9-1a4b57bce623_base_resized.jpg",
+                subdomain: "tm428",
+                name: "428tm",
+                url: "https://tm428.booth.pm/",
+                add_url: [
+                    {
+                        icon: "link",
+                        url: "twitter.com"
+                    },
+                    {
+                        icon: "link",
+                        url: "facebook.com"
+                    }
+                ]
+            }
+        };
+    },
+    methods: {
+        deleteUrl(i, url) {
+            if (this.shop.add_url[i].url != url) {
+                console.error("index and url is invalid.");
+                return;
+            }
+            this.shop.add_url.splice(i, 1);
         }
     }
 };
--- src/option/pages/CustomShopPage.vue
+++ src/option/pages/CustomShopPage.vue
@@ -3,13 +3,27 @@
         <AppBar active-text="add shop link"></AppBar>
     </div>
     <div class="content">
-        <h1>Custom Shop!</h1>
-        <h1>WIP</h1>
-        <v-row>
-            <v-col cols="12" sm="6" md="6" lg="2">
-                <ShopCard :shop="test"></ShopCard>
-            </v-col>
-        </v-row>
+        <v-container fluid>
+            <h1>Custom Shop!</h1>
+            <h1>WIP</h1>
+            <v-row>
+                <v-col cols="12" sm="6" md="6" lg="3">
+                    <ShopCard shop="test"></ShopCard>
+                </v-col>
+                <v-col cols="12" sm="6" md="6" lg="3">
+                    <ShopCard shop="test"></ShopCard>
+                </v-col>
+                <v-col cols="12" sm="6" md="6" lg="3">
+                    <ShopCard shop="test"></ShopCard>
+                </v-col>
+                <v-col cols="12" sm="6" md="6" lg="3">
+                    <ShopCard shop="test"></ShopCard>
+                </v-col>
+                <v-col cols="12" sm="6" md="6" lg="3">
+                    <ShopCard shop="test"></ShopCard>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
@@ -23,21 +37,7 @@ export default {
         ShopCard
     },
     data() {
-        return {
-            test: {
-                thumbnail_url:
-                    "https://booth.pximg.net/c/48x48/users/6511749/icon_image/53af20ae-3df9-480d-bb3d-87a413298c32_base_resized.jpg",
-                name: "ルネッパンツ",
-                nickname: "ルネッパンツ",
-                url: "https://renaipantsu.booth.pm/",
-                add_url: [
-                    {
-                        icon: "link",
-                        url: "twitter.com"
-                    }
-                ]
-            }
-        };
+        return {};
     }
 };
 </script>
