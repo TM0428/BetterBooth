0387dfd329518ecd40a9773c48ac3226e4ebdb51
t.m <takashi.orion.999.428@gmail.com>
2024-05-05 14:23:49 +0900

    shopのカードの追加

---
+++ src/option/components/ShopCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-card
+        class="d-flex flex-column"
+        height="100%"
+        color="surfaceContainerLow"
+        :title="shop.name"
+        :subtitle="shop.url"
+    >
+        <template v-slot:prepend
+            ><v-avatar start>
+                <v-img :src="shop.thumbnail_url"></v-img>
+            </v-avatar>
+        </template>
+        <v-card-text> </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        shop: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
--- src/option/pages/CustomShopPage.vue
+++ src/option/pages/CustomShopPage.vue
@@ -5,15 +5,39 @@
     <div class="content">
         <h1>Custom Shop!</h1>
         <h1>WIP</h1>
+        <v-row>
+            <v-col cols="12" sm="6" md="6" lg="2">
+                <ShopCard :shop="test"></ShopCard>
+            </v-col>
+        </v-row>
     </div>
 </template>
 
 <script>
 import AppBar from "../components/AppBar.vue";
+import ShopCard from "../components/ShopCard.vue";
 
 export default {
     components: {
-        AppBar
+        AppBar,
+        ShopCard
+    },
+    data() {
+        return {
+            test: {
+                thumbnail_url:
+                    "https://booth.pximg.net/c/48x48/users/6511749/icon_image/53af20ae-3df9-480d-bb3d-87a413298c32_base_resized.jpg",
+                name: "ルネッパンツ",
+                nickname: "ルネッパンツ",
+                url: "https://renaipantsu.booth.pm/",
+                add_url: [
+                    {
+                        icon: "link",
+                        url: "twitter.com"
+                    }
+                ]
+            }
+        };
     }
 };
 </script>
