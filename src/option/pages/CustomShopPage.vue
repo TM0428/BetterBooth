c4eff03fe85f87d9f38739e7e4bedec0ad304086
t.m <takashi.orion.999.428@gmail.com>
2024-05-09 00:42:59 +0900

    とりあえずdev用

--- src/option/pages/CustomShopPage.vue
+++ src/option/pages/CustomShopPage.vue
@@ -5,13 +5,11 @@
     <div class="content">
         <v-container fluid>
             <h1>Custom Shop!</h1>
-            <h1>WIP</h1>
             <v-row>
                 <v-col cols="12" sm="6" md="6" lg="3" v-for="(shop, i) in shops" :key="i">
                     <ShopCard :shopId="shop"></ShopCard>
                 </v-col>
             </v-row>
-            <ShopStorageTest></ShopStorageTest>
             <ShopAddPopup></ShopAddPopup>
         </v-container>
     </div>
@@ -21,15 +19,13 @@
 import { getShops } from "@/js/module/shop_data";
 import AppBar from "../components/AppBar.vue";
 import ShopCard from "../components/ShopCard.vue";
-import ShopStorageTest from "../components/ShopStorageTest.vue";
 import ShopAddPopup from "../components/ShopAddPopup.vue";
 
 export default {
     components: {
         AppBar,
         ShopCard,
-        ShopAddPopup,
-        ShopStorageTest
+        ShopAddPopup
     },
     data() {
         return {
