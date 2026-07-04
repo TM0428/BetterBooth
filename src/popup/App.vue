c81a54b3b0ddcb7d21218765e5fb1a4fa433b179
t.m <takashi.orion.999.428@gmail.com>
2026-07-04 17:58:08 +0900

    脆弱性対応とbuymecoffeeリンクの追加

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -8,6 +8,16 @@ import Option from "./pages/OptionPage.vue";
             <v-main>
                 <Option></Option>
             </v-main>
+            <v-footer app class="tip-container" color="background">
+                <a
+                    href="https://buymeacoffee.com/428_tm"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="tip-link"
+                >
+                    ☕ Do you like my extension? Buy me a coffee
+                </a>
+            </v-footer>
         </v-app>
     </div>
 </template>
@@ -18,5 +28,24 @@ import Option from "./pages/OptionPage.vue";
     margin-right: auto;
     min-width: 300px;
     width: 450px;
+}
+
+.tip-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 4px 0;
+    border-top: 1px solid rgba(128, 128, 128, 0.2);
+}
+
+.tip-link {
+    font-size: 11px;
+    color: #888;
+    text-decoration: none;
+    transition: color 0.2s;
+}
+
+.tip-link:hover {
+    color: #555;
 }
 </style>
